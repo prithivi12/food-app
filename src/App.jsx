2378d35d166c6fb7dbb9e043d0fe6b7f8e7d3a3a
prithivi12 <ankushlalchan99@gmail.com>
2024-07-10 11:48:07 +0545

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import NavBar from "./component/Navbar/NavBar";
+
+const App = () => {
+  return (
+    <div className="App">
+      <NavBar />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/component/Navbar/NavBar.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./NavBar.css";
+
+function NavBar() {
+  return <div>Hello</div>;
+}
+
+export default NavBar;
